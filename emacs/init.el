97bd3ba3614abc02ad6e00df1a8c187104b79c80
vir-thi-min <albert.tan.2.pi.0@gmail.com>
2024-01-21 19:17:06 -0300

    add folder for emacs config

---
+++ emacs/init.el
@@ -0,0 +1,15 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode -1)
+
+(menu-bar-mode -1)
+
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Ubuntu Mono" :height 200)
+
+(load-theme 'tango-dark)
+
