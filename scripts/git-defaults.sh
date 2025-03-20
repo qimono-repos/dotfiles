d04c5a9ad670436488a4b0491561f9b14a555b22
Albert.Gruning <albert.tan.2.pi.0@gmail.com>
2025-03-20 14:57:13 +0000

    feat: add file for git config  defaults

---
+++ scripts/git-defaults.sh
@@ -0,0 +1,10 @@
+
+#! /bin/bash
+
+git config --global user.email albert.tan.2.pi.0@gmail.com
+
+git config --global user.name Albert.Gruning
+
+git config --global init.defaultBranch trunk
+
+git config --global pulll.rebase true
