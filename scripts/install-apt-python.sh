0cf7898db5e41a338524f064cf8e52c8581ed81e
Albert.Gruning <albert.tan.2.pi.0@gmail.com>
2025-03-26 14:37:56 +0000

    feat: add scripts for ubuntu python packages ins ubuntu 24.10

---
+++ scripts/install-apt-python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt install emacs -y
+
+apt install neofetch -y
+
+apt install git -y
+
+apt install build-essential -y
+apt install pkg-config -y
+apt install python3-dev -y
+apt install python3-venv -y
+apt install libgirepository1.0-dev -y
+apt install libcairo2-dev -y
+apt install gir1.2-gtk-3.0 -y
+apt install libcanberra-gtk3-module -y
